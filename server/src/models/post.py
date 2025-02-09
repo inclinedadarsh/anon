c73dc80daa4ad76bd4f6ce6e5ee1eddb677c2c10
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2025-02-09 23:49:39 +0530

    feat(server): add posts to have author id
    - added middleware dependency to /posts route
    - added Post model to have an author id and a created_at field

--- server/src/models/post.py
+++ server/src/models/post.py
@@ -1,14 +1,22 @@
 from typing import Optional
 from sqlmodel import SQLModel, Field
-
+from datetime import datetime, timezone
+import sqlalchemy as sa
 
 class PostBase(SQLModel):
-    post: str
-    author: str
+    content: str
+
 
 
 class Post(PostBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    author_id: int = Field(foreign_key="user.id")
+    created_at: Optional[datetime] = Field(
+        default=None,
+        sa_type=sa.DateTime(timezone=True),
+        sa_column_kwargs={"server_default": sa.func.now()},
+        nullable=False
+    )
 
 
 class PostCreate(PostBase):
@@ -17,3 +25,5 @@ class PostCreate(PostBase):
 
 class PostPublic(PostBase):
     id: int
+    author_id: int
+    created_at: datetime
--- server/src/routes/posts.py
+++ server/src/routes/posts.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from src.models.post import PostPublic, PostCreate, Post
+from src.models.user import User
+from src.services.auth import get_current_user
 from src.db import engine
 from sqlmodel import Session, select
 from typing import List
@@ -8,9 +10,10 @@ router = APIRouter()
 
 
 @router.post("/", response_model=PostPublic)
-def post_posts(post: PostCreate):
+def post_posts(post: PostCreate, user: User = Depends(get_current_user)):
     with Session(engine) as session:
-        db_post = Post.model_validate(post)
+        post_create = Post(**post.model_dump(), author_id=user.id)
+        db_post = Post.model_validate(post_create)
         session.add(db_post)
         session.commit()
         session.refresh(db_post)
