c98356da25aa29b28c049a0c69d071427c8e6320
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2025-02-10 20:16:22 +0530

    feat(client): switch to inter font

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
 	darkMode: ["class"],
@@ -55,6 +56,9 @@ export default {
 				lg: "var(--radius)",
 				md: "calc(var(--radius) - 2px)",
 				sm: "calc(var(--radius) - 4px)",
+			},
+			fontFamily: {
+				sans: ["var(--font-sans)", ...fontFamily.sans],
 			},
 		},
 	},
